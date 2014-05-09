28f5e74aa8b68745e6efaf03536828ccab425eaf
Quentin Aupetit <quentin@cellules.tv>
2014-05-09 17:46:53 +0200

    change Base64 data structure to ArrayBuffer to be used and convert to Blob in JavaScript

--- src/android/XAPKReader.java
+++ src/android/XAPKReader.java
@@ -1,87 +1,165 @@
-var exec = require("cordova/exec");
+package org.apache.cordova.xapkreader;
 
-module.exports = {
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
-    /**
-     * Get a file in expansion file and return it as data base64 encoded
-     *
-     * @param filename              The file name
-     * @param fileType              The file type (eg: "image/jpeg")
-     * @param successCallback       The callback to be called when the file is found.
-     *                                  successCallback()
-     * @param errorCallback         The callback to be called if there is an error.
-     *                                  errorCallback(int errorCode) - OPTIONAL
-     **/
-    get: function(filename, successCallback, errorCallback, fileType) {
-        // only for android
-        if (!navigator.userAgent.match(/Android/i)) {
-            return successCallback(filename);
-        }
+import com.android.vending.expansion.zipfile.APKExpansionSupport;
+import com.android.vending.expansion.zipfile.ZipResourceFile;
+import com.google.android.vending.expansion.downloader.Helpers;
 
-        if (null === filename) {
-            console.error("XAPKReader.get failure: filename parameter needed");
-            return;
-        }
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
 
-        var context = this;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
 
-        var success = function (result) {
-            try {
-                var url = context.arrayBufferToURL(result, fileType);
-                successCallback(url);
+public class XAPKReader extends CordovaPlugin {
+
+    private static final String LOG_TAG = "XAPKReader";
+
+    private int mainVersion = 1;
+
+    private int patchVersion = 1;
+
+    private long fileSize = 0L;
+
+    @Override
+    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+
+        int mainVersionId = cordova.getActivity().getResources().getIdentifier("main_version", "integer", cordova.getActivity().getPackageName());
+        mainVersion = cordova.getActivity().getResources().getInteger(mainVersionId);
+
+        int patchVersionId = cordova.getActivity().getResources().getIdentifier("patch_version", "integer", cordova.getActivity().getPackageName());
+        patchVersion = cordova.getActivity().getResources().getInteger(patchVersionId);
+
+        int fileSizeId = cordova.getActivity().getResources().getIdentifier("file_size", "integer", cordova.getActivity().getPackageName());
+        fileSize = cordova.getActivity().getResources().getInteger(fileSizeId);
+
+        final Bundle bundle = new Bundle();
+        bundle.putInt("mainVersion", mainVersion);
+        bundle.putInt("patchVersion", patchVersion);
+        bundle.putLong("fileSize", fileSize);
+
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Context context = cordova.getActivity().getApplicationContext();
+                Intent intent = new Intent(context, XAPKDownloaderActivity.class);
+                intent.putExtras(bundle);
+                cordova.getActivity().startActivity(intent);
             }
-            catch (e) {
-                errorCallback(e);
-            }
-        };
+        });
 
-        cordova.exec(success, errorCallback, "XAPKReader", "get", [filename]);
-    },
-
-    /**
-     * Convert ArrayBuffer to URL
-     *
-     * @param arrayBuffer   ArrayBuffer to convert
-     * @param fileType      (optional) The file type (eg: "image/jpeg")
-     * @return URL          URL string
-     **/
-    arrayBufferToURL: function (arrayBuffer, fileType) {
-        var blob = this.createBlob(arrayBuffer);
-        window.URL = window.URL || window.webkitURL;
-        return window.URL.createObjectURL(blob);
-    },
-
-    /**
-     * Create Blob from data
-     *
-     * @param part          ArrayBuffer, ArrayBufferView, Blob or DOMString part
-     * @param fileType      (optional) The file type (eg: "image/jpeg")
-     **/
-    createBlob: function (part, fileType) {
-        var blob;
-        try {
-            var properties = {};
-            if (fileType) {
-                properties.type = fileType;
-            }
-            blob = new Blob([part], properties);
-        }
-        catch (e) {
-            // TypeError try old constructor
-            window.BlobBuilder = window.BlobBuilder ||
-                window.WebKitBlobBuilder ||
-                window.MozBlobBuilder ||
-                window.MSBlobBuilder;
-
-            if (e.name == 'TypeError' && !window.BlobBuilder) {
-                throw new Error('This platform does not support Blob type.');
-            }
-
-            var bb = new BlobBuilder();
-            bb.append(part);
-            blob = bb.getBlob(fileType);
-        }
-        return blob;
+        super.initialize(cordova, webView);
     }
 
-};
+    /**
+     * Executes the request.
+     *
+     * This method is called from the WebView thread. To do a non-trivial amount of work, use:
+     *     cordova.getThreadPool().execute(runnable);
+     *
+     * To run on the UI thread, use:
+     *     cordova.getActivity().runOnUiThread(runnable);
+     *
+     * @param action          The action to execute.
+     * @param args            The exec() arguments.
+     * @param callbackContext The callback context used when calling back into JavaScript.
+     * @return                Whether the action was valid.
+     * @throws JSONException
+     *
+     * @sa https://github.com/apache/cordova-android/blob/master/framework/src/org/apache/cordova/CordovaPlugin.java
+     */
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        if (action.equals("get")) {
+            final String filename = args.getString(0);
+            final Context ctx = cordova.getActivity().getApplicationContext();
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    try {
+                        // Read file
+                        PluginResult result = XAPKReader.readFile(ctx, filename, mainVersion, patchVersion, PluginResult.MESSAGE_TYPE_ARRAYBUFFER);
+                        callbackContext.sendPluginResult(result);
+                    }
+                    catch(Exception e) {
+                        e.printStackTrace();
+                        callbackContext.error(e.getLocalizedMessage());
+                    }
+                }
+            });
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Read file in APK Expansion file.
+     *
+     * @param ctx      The context of the main Activity.
+     * @param filename The filename to read
+     * @return         PluginResult
+     */
+    private static PluginResult readFile(Context ctx, String filename, int mainVersion, int patchVersion, final int resultType) throws IOException {
+        // Get APKExpensionFile
+        ZipResourceFile expansionFile = APKExpansionSupport.getAPKExpansionZipFile(ctx, mainVersion, patchVersion);
+
+        if (null == expansionFile) {
+            Log.e(LOG_TAG, "APKExpansionFile not found.");
+            return null;
+        }
+
+        // Find file in ExpansionFile
+        String fileName = Helpers.getExpansionAPKFileName(ctx, true, patchVersion);
+        fileName = fileName.substring(0, fileName.lastIndexOf("."));
+        AssetFileDescriptor fileDescriptor = expansionFile.getAssetFileDescriptor(fileName + "/" + filename);
+
+        if (null == fileDescriptor) {
+            Log.e(LOG_TAG, "File not found (" + filename + ").");
+            return null;
+        }
+
+        InputStream inputStream = fileDescriptor.createInputStream();
+        String contentType = URLConnection.guessContentTypeFromStream(inputStream);
+
+        // // Read file
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int read = 0;
+        while ((read = inputStream.read(buffer, 0, buffer.length)) != -1) {
+            os.write(buffer, 0, read);
+        }
+        os.flush();
+
+        PluginResult result;
+        switch (resultType) {
+            case PluginResult.MESSAGE_TYPE_STRING:
+                result = new PluginResult(PluginResult.Status.OK, os.toString("UTF-8"));
+                break;
+            case PluginResult.MESSAGE_TYPE_ARRAYBUFFER:
+                result = new PluginResult(PluginResult.Status.OK, os.toByteArray());
+                break;
+            case PluginResult.MESSAGE_TYPE_BINARYSTRING:
+                result = new PluginResult(PluginResult.Status.OK, os.toByteArray(), true);
+                break;
+            default: // Base64.
+                byte[] base64 = Base64.encode(os.toByteArray(), Base64.NO_WRAP);
+                String s = "data:" + contentType + ";base64," + new String(base64, "US-ASCII");
+                result = new PluginResult(PluginResult.Status.OK, s);
+        }
+
+        return result;
+    }
+
+}
--- www/xapkreader.js
+++ www/xapkreader.js
@@ -1,55 +1,87 @@
 var exec = require("cordova/exec");
 
-/**
- * Constructor
- **/
-var XAPKReader = function() {};
+module.exports = {
 
-/**
- * Get a file in expansion file and return it as data base64 encoded
- *
- * @param filename              The file name
- * @param fileType              The file type (eg: "image/jpeg")
- * @param successCallback       The callback to be called when the file is found.
- *                                  successCallback()
- * @param errorCallback         The callback to be called if there is an error.
- *                                  errorCallback(int errorCode) - OPTIONAL
- **/
-XAPKReader.prototype.get = function(filename, fileType, successCallback, errorCallback) {
-    // only for android
-    if (!navigator.userAgent.match(/Android/i)) {
-        return successCallback(filename);
-    }
-
-    if (null === filename) {
-        console.error("XAPKReader.get failure: filename parameter needed");
-        return;
-    }
-
-    var context = this;
-
-    cordova.exec(
-        function (result) {
-            context.arrayBufferToURL(result, fileType, successCallback, errorCallback);
-        },
-        errorCallback, "XAPKReader", "get", [filename]
-    );
-};
-
-XAPKReader.prototype.arrayBufferToURL = function (arrayBuffer, fileType, successCallback, errorCallback) {
-    try {
-        var properties = {};
-        if (fileType) {
-            properties.type = fileType;
+    /**
+     * Get a file in expansion file and return it as data base64 encoded
+     *
+     * @param filename              The file name
+     * @param fileType              The file type (eg: "image/jpeg")
+     * @param successCallback       The callback to be called when the file is found.
+     *                                  successCallback()
+     * @param errorCallback         The callback to be called if there is an error.
+     *                                  errorCallback(int errorCode) - OPTIONAL
+     **/
+    get: function(filename, successCallback, errorCallback, fileType) {
+        // only for android
+        if (!navigator.userAgent.match(/Android/i)) {
+            return successCallback(filename);
         }
-        var blob = new Blob([arrayBuffer], properties);
-        window.URL = window.URL || window.webkitURL;
-        var url = window.URL.createObjectURL(blob);
-        successCallback.call(this, url);
-    }
-    catch (error) {
-        errorCallback.call(this, error);
-    }
-};
 
-module.exports = new XAPKReader();
+        if (null === filename) {
+            console.error("XAPKReader.get failure: filename parameter needed");
+            return;
+        }
+
+        var context = this;
+
+        var success = function (result) {
+            try {
+                var url = context.arrayBufferToURL(result, fileType);
+                successCallback(url);
+            }
+            catch (e) {
+                errorCallback(e);
+            }
+        };
+
+        cordova.exec(success, errorCallback, "XAPKReader", "get", [filename]);
+    },
+
+    /**
+     * Convert ArrayBuffer to URL
+     *
+     * @param arrayBuffer   ArrayBuffer to convert
+     * @param fileType      (optional) The file type (eg: "image/jpeg")
+     * @return URL          URL string
+     **/
+    arrayBufferToURL: function (arrayBuffer, fileType) {
+        var blob = this.createBlob(arrayBuffer);
+        window.URL = window.URL || window.webkitURL;
+        return window.URL.createObjectURL(blob);
+    },
+
+    /**
+     * Create Blob from data
+     *
+     * @param part          ArrayBuffer, ArrayBufferView, Blob or DOMString part
+     * @param fileType      (optional) The file type (eg: "image/jpeg")
+     **/
+    createBlob: function (part, fileType) {
+        var blob;
+        try {
+            var properties = {};
+            if (fileType) {
+                properties.type = fileType;
+            }
+            blob = new Blob([part], properties);
+        }
+        catch (e) {
+            // TypeError try old constructor
+            window.BlobBuilder = window.BlobBuilder ||
+                window.WebKitBlobBuilder ||
+                window.MozBlobBuilder ||
+                window.MSBlobBuilder;
+
+            if (e.name == 'TypeError' && !window.BlobBuilder) {
+                throw new Error('This platform does not support Blob type.');
+            }
+
+            var bb = new BlobBuilder();
+            bb.append(part);
+            blob = bb.getBlob(fileType);
+        }
+        return blob;
+    }
+
+};
