5c4de2ce0891edcbc5dbd143fed684e657f0b5a5
Quentin Aupetit <quentin@cellules.tv>
2014-05-09 17:03:20 +0200

    change Base64 data structure to ArrayBuffer to be used and convert to Blob in JavaScript

--- www/xapkreader.js
+++ www/xapkreader.js
@@ -9,12 +9,13 @@ var XAPKReader = function() {};
  * Get a file in expansion file and return it as data base64 encoded
  *
  * @param filename              The file name
+ * @param fileType              The file type (eg: "image/jpeg")
  * @param successCallback       The callback to be called when the file is found.
  *                                  successCallback()
  * @param errorCallback         The callback to be called if there is an error.
  *                                  errorCallback(int errorCode) - OPTIONAL
  **/
-XAPKReader.prototype.get = function(filename, successCallback, errorCallback) {
+XAPKReader.prototype.get = function(filename, fileType, successCallback, errorCallback) {
     // only for android
     if (!navigator.userAgent.match(/Android/i)) {
         return successCallback(filename);
@@ -25,7 +26,30 @@ XAPKReader.prototype.get = function(filename, successCallback, errorCallback) {
         return;
     }
 
-    cordova.exec(successCallback, errorCallback, "XAPKReader", "get", [filename]);
+    var context = this;
+
+    cordova.exec(
+        function (result) {
+            context.arrayBufferToURL(result, fileType, successCallback, errorCallback);
+        },
+        errorCallback, "XAPKReader", "get", [filename]
+    );
+};
+
+XAPKReader.prototype.arrayBufferToURL = function (arrayBuffer, fileType, successCallback, errorCallback) {
+    try {
+        var properties = {};
+        if (fileType) {
+            properties.type = fileType;
+        }
+        var blob = new Blob([arrayBuffer], properties);
+        window.URL = window.URL || window.webkitURL;
+        var url = window.URL.createObjectURL(blob);
+        successCallback.call(this, url);
+    }
+    catch (error) {
+        errorCallback.call(this, error);
+    }
 };
 
 module.exports = new XAPKReader();
